8b7e330541ecc29a69ba2ef1fcb0d25bfbcb1b2d
Michal Sládeček <michal.sladecek89@gmail.com>
2022-02-21 18:04:24 +0100

    Edited header badges added Search box to navigation.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,29 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators, FormGroup } from '@angular/forms';
 
-
-
-
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  validatingForm: FormGroup;
 
-  ngOnInit() {
-    this.validatingForm = new FormGroup({
-      loginFormModalEmail: new FormControl('', Validators.email),
-      loginFormModalPassword: new FormControl('', Validators.required)
-    });
-  }
+  name = 'Angular';
 
-  get loginFormModalEmail() {
-    return this.validatingForm.get('loginFormModalEmail');
-  }
-
-  get loginFormModalPassword() {
-    return this.validatingForm.get('loginFormModalPassword');
-  }
 }
+  
