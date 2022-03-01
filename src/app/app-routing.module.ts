1e80f95941489cd770ebca110fdfd248a7f266e6
simon.kvasnovsky@gmail.com <simon.kvasnovsky@gmail.com>
2022-03-01 12:54:20 +0100

    Add basic view login signup.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
 import { MainblogComponent } from './mainblog/mainblog.component';
+import { LoginComponent } from './service/login/login.component';
+import { RegisterComponent } from './service/register/register.component';
 
 const routes: Routes = [
   { path: '', component: MainblogComponent},
   { path: 'mainblog', component: MainblogComponent},
   { path: 'contact', component: ContactComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
   
 ];
 
