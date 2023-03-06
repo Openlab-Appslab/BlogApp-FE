0b81b7af04703fd2c146d207146daa5d7da47b45
simon <simon.kvasnovsky@gmail.com>
2023-03-06 19:26:25 +0100

    Complete merge. Edit Pages.

--- src/app/blog-detail/blog-detail.component.ts
+++ src/app/blog-detail/blog-detail.component.ts
@@ -1,54 +1,18 @@
-import { Component, OnInit, Input, ViewEncapsulation  } from '@angular/core';
-// import { Blog } from '../blog';
+import { Component, OnInit } from '@angular/core';
 import { Blog } from '../blog.model';
-import { ShowPostService } from '../website/service/show-post.service';
+import { BlogDetailService } from '../service/blog-detail.service';
 import { ActivatedRoute } from '@angular/router';
-import { CommonService } from '../website/service/common.service';
-import { SafeHtmlPipe } from '../safe-html.pipe';
-// importovať observable import { Observable } from 'rxjs';
-import { observable, Observable } from 'rxjs';
-// import { SSL_OP_NETSCAPE_REUSE_CIPHER_CHANGE_BUG } from 'constants';
 
 @Component({
   selector: 'app-blogs',
-  templateUrl: './blog-detail.component.html',
-  styleUrls: ['./blog-detail.component.css']
 })
 export class BlogDetailComponent implements OnInit {
 
-  // pridať public blog$: Observable<Blog>;
-  public blog$: Observable<Blog>;
+  constructor(private blogService: BlogDetailService, private route: ActivatedRoute) { }
 
-  constructor(
-    private showPostService: ShowPostService, 
-    private route: ActivatedRoute,
-    private commonService: CommonService
-    ) { 
-    }
-
-    ngOnInit(){
-      this.getBlogDetail();
-  
-      // this.commonService.blogAdded_Observable.subscribe(res => {
-      //   this.getBlog();
-      // });
-
-      // this.showPostService.getBlogDetail(this.route.snapshot.paramMap.get('id')).subscribe(blog => this.blog = blog);
-    }
-  
-    getBlogDetail(){
-
-      // subscribe netreba, ngif rozhodne, či treba subsribovat
-      this.blog$ = this.showPostService.getBlogDetail(this.route.snapshot.paramMap.get('id'));
-    }
-
-
-    selectedDate: any;
-
-     onSelect(event){
-      console.log(event);
-      this.selectedDate = event;
-     }
+  blog: Blog;
+  ngOnInit(): void {
+    this.blogService.getBlogDetail(this.route.snapshot.paramMap.get('id')).subscribe(blog => this.blog = blog);
+ }
 
 }
-  
